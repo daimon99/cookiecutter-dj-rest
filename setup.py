a58e7ae9c4e637b3513a7b655f7f1a9fb5441122
daijian@air <daijian@flowself.com>
2018-07-05 18:17:02 +0800

    init

---
+++ setup.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-django',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for a django project',
+    author='daimon99',
+    license='BSD',
+    author_email='daijian1@qq.com',
+    url='https://github.com/daimon99/cookiecutter-django',
+    keywords=['cookiecutter', 'template', 'package', 'django', ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
