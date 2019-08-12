39476fbd49a7d1f21cae4deefd35b6a7b008b115
戴健 <daimon@daijiandeMacBook-Air-2.local>
2019-08-12 21:50:15 +0800

    fix bug

--- {{cookiecutter.project_slug}}/src/config/settings.py
+++ {{cookiecutter.project_slug}}/src/config/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'dapiconfig.context_processors.footer',
+                'config.context_processors.footer',
             ],
         },
     },
