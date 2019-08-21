178d820a4a8a8a9c48499f230525ed04962bfdf9
戴健 <daimon@daijiandeMacBook-Air-2.local>
2019-08-21 13:40:40 +0800

    补充两个遗忘的配置template dir 与static dir

--- {{cookiecutter.project_slug}}/src/config/settings.py
+++ {{cookiecutter.project_slug}}/src/config/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,6 +136,9 @@ formats.NUMBER_GROUPING = 3
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
