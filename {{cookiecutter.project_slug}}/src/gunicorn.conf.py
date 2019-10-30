9a178622161e38cb61effec882573132c42235d1
戴健 <daimon@daijiandeMacBook-Air-2.local>
2019-10-30 18:50:27 +0800

    add sentry and telegraf

--- {{cookiecutter.project_slug}}/src/gunicorn.conf.py
+++ {{cookiecutter.project_slug}}/src/gunicorn.conf.py
@@ -1,8 +1,7 @@
 # coding: utf-8
-import multiprocessing
 
 bind = "127.0.0.1:{{cookiecutter.port}}"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 4
 loglevel = "info"
 proc_name = "{{cookiecutter.project_slug}}"
 worker_class = "gevent"
