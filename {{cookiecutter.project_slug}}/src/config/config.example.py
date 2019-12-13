a624d33e023f0f86685624850b5413fccf6286be
daijian <daijian1@qq.com>
2019-12-13 13:09:49 +0800

    update

--- {{cookiecutter.project_slug}}/src/config/config.example.py
+++ {{cookiecutter.project_slug}}/src/config/config.example.py
@@ -1,4 +1,13 @@
 # coding: utf-8
 # 经常调整的配置文件参数
 
-debug = True
+import environ
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+TJHB_DEBUG = env.bool('TJHB_DEBUG', default=False)
+SECRET_KEY = env.bool('SECRET_KEY', default='_uh0jj8&ge&xp_0^*n&ms_@)72pzlmx99-=4!7#esgdiq@%&#k')
--- {{cookiecutter.project_slug}}/src/config/settings.py
+++ {{cookiecutter.project_slug}}/src/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_uh0jj8&ge&xp_0^*n&ms_@)72pzlmx99-=4!7#esgdiq@%&#k'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.TJHB_DEBUG
