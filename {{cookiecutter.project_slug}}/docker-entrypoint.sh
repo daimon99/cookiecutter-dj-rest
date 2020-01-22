b8a8b60134c12457d2ece9e4154099d99828294b
daijian <daijian1@qq.com>
2020-01-22 15:16:58 +0800

    update

--- {{cookiecutter.project_slug}}/docker-entrypoint.sh
+++ {{cookiecutter.project_slug}}/docker-entrypoint.sh
@@ -29,7 +29,7 @@ case "$1" in
     runserver)
         echo "Running Development Server..."
         pip3 install -r requirements-docker.txt
-        python3 src/manage.py migrate || python3 cli.py fail; exit 1
+        python3 src/manage.py migrate || (python3 cli.py fail; exit 1)
         python3 src/manage.py loaddata --format yaml fixtures.yaml
         python3 src/manage.py collectstatic --noinput
         python3 cli.py ok
