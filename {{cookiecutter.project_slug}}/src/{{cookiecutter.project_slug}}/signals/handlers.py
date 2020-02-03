783b0543db6bc440b1b543fbc795078930cdd615
daimon@imac <daijian1@qq.com>
2020-02-04 01:59:08 +0800

    大幅度调整代码。没有经过测试。下次使用模板生成项目的时候再考虑解决bug

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/signals/handlers.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/signals/handlers.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 
 @receiver(post_save, sender=User)
 def save_user(sender, instance: User, created, **kwargs):
-	"""这里可以用来创建 User 的相关对象。如 One2One 的那些模型数据"""
+    """这里可以用来创建 User 的相关对象。如 One2One 的那些模型数据"""
     if not created:
         return
     log.warning('新建用户：%s', instance)
